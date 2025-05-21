f673307fb00e176e4dcd354121eda69ed7afa932
NoneStoptes <NoneStoptes@gmail.com>
2025-05-21 20:47:28 +0300

    Добавлен проект для тестов некоторых функций

---
+++ MauiTest/MauiTest/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiTest
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Nickname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
---
+++ MauiTest/MauiTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using Firebase.Auth;
+using Firebase.Auth.Providers;
+using Firebase.Auth.Repository;
+using Firebase.Database;
+using Firebase.Database.Query;
+using System;
+using System.Threading.Tasks;
+
+namespace MauiTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            FirebaseServices.Init(); // Initialize Firebase services
+
+            Console.WriteLine("Enter your nickname: ");
+            string Nickname = Console.ReadLine();
+            Console.WriteLine("Enter your email: ");
+            string Email = Console.ReadLine();
+
+
+
+            FirebaseServices service = new FirebaseServices();
+            bool isNickNameTaken = await service.IsNicknameExists(Nickname);
+            bool isEmailTaken = await service.IsEmailExists(Email);
+
+            Console.WriteLine($"Is NickName taken: {isNickNameTaken}\nIs Email taken: {isEmailTaken}");
+        }
+    }
+}
